f6858d6b7034ed8d93bda1909d2d7799df24904f
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-04-10 18:20:24 +0300

    add cards to display quotes

--- src/app/form/quotes/quotes.component.ts
+++ src/app/form/quotes/quotes.component.ts
@@ -7,13 +7,13 @@ import { Quotes } from 'src/app/quotes';
 })
 export class QuotesComponent implements OnInit {
 
-  quote:Quotes[]=[
-    new Quotes('kelly','dont give up', 0, 0),
-    new Quotes('kiiru','life is what you make it', 0, 0),
-    new Quotes('kelly','there is always a silver lining', 0, 0),
-    new Quotes('kelly','never look down on yourself', 0, 0),
-    new Quotes('kelly','you have all it takes', 0, 0)
-  ]
+  // quote:Quotes[]=[
+  //   new Quotes('kelly','dont give up', 0, 0),
+  //   new Quotes('kiiru','life is what you make it', 0, 0),
+  //   new Quotes('kelly','there is always a silver lining', 0, 0),
+  //   new Quotes('kelly','never look down on yourself', 0, 0),
+  //   new Quotes('kelly','you have all it takes', 0, 0)
+  // ]
   constructor() { }
 
   ngOnInit(): void {
