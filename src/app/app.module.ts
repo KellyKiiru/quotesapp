884f04af573ee815f52396d9b82f890d90fa08d3
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-04-10 10:17:45 +0300

    add quotes component and edit form-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormCmpntComponent } from './form/form-cmpnt/form-cmpnt.component';
+import { QuotesComponent } from './form/quotes/quotes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormCmpntComponent
+    FormCmpntComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
